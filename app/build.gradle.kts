9ec0f686fb72d8be8b510368ef9b0a8a886d3df2
lyne05 <evelyne.natalie2705@gmail.com>
2025-11-25 17:36:59 +0700

    Photo and Video using FileProvider and MediaStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.commons.io)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
